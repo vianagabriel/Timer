6b449750b9ccd011aa749455629046eb85beec6b
vianagabriel <vianagabrieldacunha@gmail.com>
2022-05-02 16:08:49 -0300

    subindo timer

---
+++ main.js
@@ -0,0 +1,38 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function criaHoraDosSegundos (segundos){
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
+
+function inciaRelogio(){
+   timer = setInterval(function(){
+   segundos++;
+   relogio.innerHTML = criaHoraDosSegundos(segundos);
+  }, 1000)
+}
+
+iniciar.addEventListener('click', function(event) {
+  clearInterval(timer);
+  inciaRelogio()
+});
+
+pausar.addEventListener('click', function (event){
+  clearInterval(timer);
+})
+
+zerar.addEventListener('click', function(event) {
+  clearInterval(timer);
+  relogio.innerHTML = '00:00:00'
+  segundos =0;
+})
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+<h1>Timer</h1>
+<p>Projeto realizada para treinar conhecimentos em JavaScript, não focando muito na estilização.</p>
+
+<h3>Tecnologias Utilizadas</h3>
+<p>Html5, Css3 e Javascript<p>
+
